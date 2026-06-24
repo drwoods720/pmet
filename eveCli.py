eb0273ed73292aa31f7ff5638d7c3f2b8843800c
Dylan Woods <drwoods720@gmail.com>
2026-06-24 13:09:13 -0700

    Started development on cli ui

---
+++ eveCli.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+import click
+import src.run as run
+
+title: str = r'''
+ _____      _____
+|  ___|    |  ___|
+| |____   _| |__
+|  __\ \ / /  __|
+| |___\ V /| |___
+\____(_)_(_)____/
+(EvE)aluates Various modEls
+'''
